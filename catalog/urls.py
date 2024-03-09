cc94bae3fe67287ec3703099dc766ffff268134c
Jean <acalto2@gmail.com>
2024-03-09 17:32:21 -0500

    auth,perms

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path("books/", views.BookListView.as_view(), name="books"),
     path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
 ]
+urlpatterns += [
+    path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
+]
